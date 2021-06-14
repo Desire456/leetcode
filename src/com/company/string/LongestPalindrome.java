f6505dcaece7dc98978a7777c897720ecd0e11bf
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-06-14 15:31:51 +0400

    Longest Palindrome problem (https://leetcode.com/problems/longest-palindromic-substring/) was solved

---
+++ src/com/company/string/LongestPalindrome.java
@@ -0,0 +1,26 @@
+package com.company.string;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        int startPos = 0;
+        int endPos = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int length1 = palindromeLength(s, i, i);
+            int length2 = palindromeLength(s, i, i + 1);
+            int length = Math.max(length1, length2);
+            if (length > endPos - startPos) {
+                startPos = i - (length - 1) / 2;
+                endPos = i + length / 2;
+            }
+        }
+        return s.substring(startPos, endPos + 1);
+    }
+
+    private int palindromeLength(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+}
---
+++ src/com/company/string/Main.java
@@ -0,0 +1,12 @@
+package com.company.string;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(longestPalindrome.longestPalindrome(input));
+    }
+}
