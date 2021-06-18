598605eee3d2441ba6235cef256975b32b8aaaa8
Vladislav Selitskii <selitskiivd@haulmont.com>
2021-06-18 21:39:16 +0400

    The problem "7. Reverse Integer" was solved
    https://leetcode.com/problems/reverse-integer/

---
+++ src/com/company/other/reverseinteger/Main.java
@@ -0,0 +1,10 @@
+package com.company.other.reverseinteger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(new Solution().reverse(scanner.nextInt()));
+    }
+}
