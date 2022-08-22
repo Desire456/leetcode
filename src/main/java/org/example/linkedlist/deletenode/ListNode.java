2a5421f2d66728e0a7edd468c0f5f67f0f1c86c9
Vladislav Selitskii <selickiy.vladislav@gmail.com>
2022-08-23 00:49:36 +0400

    Delete Node in a Linked List
    https://leetcode.com/problems/delete-node-in-a-linked-list/

---
+++ src/main/java/org/example/linkedlist/deletenode/ListNode.java
@@ -0,0 +1,10 @@
+package org.example.linkedlist.deletenode;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/test/java/org/example/linkedlist/deletenode/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.example.linkedlist.deletenode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    @Test
+    public void deleteNode() {
+        var listNode1 = new ListNode(4);
+        var listNode2 = new ListNode(5);
+        var listNode3 = new ListNode(1);
+        var listNode4 = new ListNode(9);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+
+        new Solution().deleteNode(listNode2);
+
+        int[] expectedValues = {4, 1, 9};
+        int i = 0;
+        var current = listNode1;
+        while (current.next != null) {
+            assertEquals(expectedValues[i], current.val);
+            ++i;
+            current = current.next;
+        }
+    }
+}
